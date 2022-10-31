b1b7db87a4f4e09abfa7c2669d1dd36f9a48310f
Archie <archie.hamilton@eh9.co.uk>
2022-10-31 16:05:55 +0000

    allow player choice, compare, show results

---
+++ main.js
@@ -0,0 +1,82 @@
+/*set global variables*/
+const choices = ["rock", "paper", "scissors"]
+const winners = []
+
+/*play game*/
+function playGame(){
+    console.clear();
+    for(let i = 0; i < 5; i++){
+        playRound(i);
+    }
+    document.querySelector("button").textContent = "Start new game"
+    logWinners()
+}
+/*play round*/;
+function playRound(round){
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+    const winner = checkWinner(playerSelection, computerSelection);
+    winners.push(winner);
+    logRound(playerSelection, computerSelection, winner, round);
+}
+/*get computer choice*/
+function computerChoice(){
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+/*get player choice*/
+function playerChoice(){
+    let input = prompt("Choose Rock, Paper or Scissors");
+    while (input == null){
+        input = prompt("Choose Rock, Paper or Scissors");
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    while (check == false){
+        input = prompt("Please select valid choice, capitalisation is not important however spelling must be correct");
+        while (input == null){
+            input = prompt("Please select valid choice, capitalisation is not important however spelling must be correct");
+        }
+        input = input.toLowerCase();
+        check = validateInput(input);
+    }
+    return input;
+}
+
+/*validate player choice*/
+function validateInput(choice){
+    return choices.includes(choice);
+}
+
+/*compare choices and set winner*/
+function checkWinner(choicePlayer, choiceComputer){
+    if (choicePlayer === choiceComputer){
+        return "This round was a tie";
+    }
+    else if ((choicePlayer == "rock" && choiceComputer == "scissors") ||
+             (choicePlayer == "paper" && choiceComputer == "rock") ||
+             (choicePlayer == "scissors" && choiceComputer == "paper")){
+        return "You have won this round";
+    }
+    else {
+        return "The computer won this round";
+    }
+}
+/*display the results*/
+function logWinners(){
+    let playerWins = winners.filter((winner) => winner == "You have won this round").length;
+    let computerWins = winners.filter((winner) => winner == "The computer won this round").length;
+    let gameTies = winners.filter((winner) => winner == "This round was a tie").length;
+    console.log("Results:");
+    console.log("Player won: ", playerWins, " time(s)");
+    console.log("Computer won: ", computerWins, " time(s)");
+    console.log("The game was tied ", gameTies, " time(s)");
+}
+
+function logRound(playerChoice, computerChoice, winner, round){
+    console.log("Round: ", round);
+    console.log("Player chose: ", playerChoice);
+    console.log("Computer chose: ", computerChoice);
+    console.log(winner, " Won this round");
+    console.log("--------------------------");
+}
